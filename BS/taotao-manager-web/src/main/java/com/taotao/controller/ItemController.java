8c35208a042bf9158578adbb875420ea4b3276a2
hy <1071324110@qq.com>
2018-01-18 22:16:14 +0800

    后台页面跳转

--- BS/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ BS/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,5 +1,6 @@
 package com.taotao.controller;
 
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,11 @@ public class ItemController {
         return tbItem;
     }
 
+    @RequestMapping("/item/list")
+    @ResponseBody
+    public EasyUIDataGridResult ItemList(Integer page,Integer rows){
+        EasyUIDataGridResult result = itemService.getItemList(page, rows);
+        return result;
+    }
 
 }
