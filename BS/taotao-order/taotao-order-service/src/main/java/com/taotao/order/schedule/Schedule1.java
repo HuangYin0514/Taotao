d0fe80e404cea5ad0c5e6a99147ee43aa886cb59
hy <1071324110@qq.com>
2018-02-05 23:33:55 +0800

    增加quartz定时器功能

---
+++ BS/taotao-order/taotao-order-service/src/main/java/com/taotao/order/schedule/Schedule1.java
@@ -0,0 +1,10 @@
+package com.taotao.order.schedule;
+
+import java.util.Date;
+
+public class Schedule1 {
+
+    public void exec() {
+        System.out.println("Schedule1 exec by quartz~~~~"+new Date());
+    }
+}
